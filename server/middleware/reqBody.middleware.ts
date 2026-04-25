bfcb7a46fc3cadd06b8dc65d66289f3acc6a7369
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2026-04-25 20:08:51 +0530

    feat: minor changes

--- server/middleware/reqBody.middleware.ts
+++ server/middleware/reqBody.middleware.ts
@@ -1,12 +1,9 @@
 import { Controller } from '../lib/types/controller.types';
+import { ApiError, ErrorCode } from '../lib/utils/error.util';
 
 const reqBodyMiddleware: Controller = (req, res, next) => {
   if (!req.body || Object.keys(req.body).length === 0) {
-    res.status(400).json({
-      success: false,
-      message: 'Request body is empty',
-    });
-    return;
+    throw new ApiError(ErrorCode.BAD_REQUEST, 'Request body is empty');
   }
   next();
 };
