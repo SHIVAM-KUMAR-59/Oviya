16e3a9bfd731c61754a053371666e48cc4b70b87
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2026-04-25 15:49:29 +0530

    feat: Auth page ready

--- client/components/auth/Header.tsx
+++ client/components/auth/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Reveal from './Reveal';
 
 const Header = () => {
@@ -5,13 +6,13 @@ const Header = () => {
     <Reveal delay={0}>
       <div className="mb-7 text-center">
         <div className="inline-flex items-center gap-2.5">
-          <div className="h-px w-7 bg-linear-to-r from-transparent to-rose" />
-
-          <span className="font-[Cormorant,serif] text-[30px] font-semibold text-ink">
+          <div className="to-rose h-px w-7 bg-linear-to-r from-transparent" />
+          <Image src={'/logo.png'} alt={'logo'} height={50} width={50} />
+          <span className="text-ink font-[Cormorant,serif] text-[30px] font-semibold">
             Ovi<em className="text-violet italic">ya</em>
           </span>
 
-          <div className="h-px w-7 bg-linear-to-l from-transparent to-rose" />
+          <div className="to-rose h-px w-7 bg-linear-to-l from-transparent" />
         </div>
       </div>
     </Reveal>
